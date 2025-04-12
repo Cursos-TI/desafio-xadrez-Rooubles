4d790dfdd41f36ce3facc2388aeb8a635ec862a0
Rooubles <mario.roubles@gmail.com>
2025-04-12 03:34:39 +0000

    lops simples substituidos por procedimentos recurcivos para cada peça

--- xadrez.c
+++ xadrez.c
@@ -1,64 +1,57 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck (Novato)
- 
+void torre (int casas) {  // procedimeto que define o movimento da torre
+    if (casas > 0) {  // condição para execução do procedimento
+        printf ("direita\n");  // imprime o sentido que a peça vai se mover
+        torre (casas -1 );  // recursiva que possibilitará repedir o procedimento de acordo com a quantidade de movimentos que peça executará   
+    }
+}
+
+void bispo (int casas) { // procedimeto que define o movimento do bispo
+    if (casas > 0 ){  // condição para execução do procedimento
+        for(int vertical = 1 ; vertical <=1 ; vertical++ ){  // loop externo que define o movimento vertical
+            printf ("Cima, ");  // imprime o sentido vertical que a peça vai se mover
+            for (int horizontal = 1 ; horizontal <= 1 ; horizontal++) { // loop interno que define o movimento horizontal
+                printf ("Direita\n");  // imprime o sentido horizontal que a peça vai se mover
+            }
+        }
+        bispo(casas - 1 );  // recursiva que possibilitará repedir o procedimento de acordo com a quantidade de movimentos que peça executará
+    }       
+}
+
+void rainha (int casas) { // procedimeto que define o movimento da rainha
+    if (casas > 0) {  // condição para execução do procedimento
+        printf ("esquerda\n"); // imprime o sentido que a peça vai se mover
+        rainha (casas -1); // recursiva que possibilitará repedir o procedimento de acordo com a quantidade de movimentos que peça executará
+    } 
+}
+
+void cavalo (int casas) { // procedimeto que define o movimento do cavalo
+    if (casas > 0 ) {  // condição para execução do procedimento
+        for ( int i = 1 , j = 1 ; i <= 2 ; i++ , j++ ) {  // i para primeira parte do movimento e j para a segunda.
+            printf ("Cima\n"); // imprime o sentido que a peça vai se mover
+            if (j % 2 == 0 ) { printf ("Direita\n"); } // imprime o sentido que a peça vai se mover
+        }
+        cavalo (casas - 1);  // recursiva que possibilitará repedir o procedimento de acordo com a quantidade de movimentos que peça executará
+    }     
+}
+
 int main() {
 
-    // MOVIMENTAÇÂO DA TORRE - ESTRUTURA DE REPETIÇAO "FOR"
-
     printf ("\nTORRE cinco casas para a direita.\n");
-    
-    for ( int torre = 1 ; torre <= 5 ; torre++) {  // declaração da variavel ocorre dentro da propria estrutura, porém seu uso fica restrito a ela.
-        printf ("Direita\n");        
-    }
+        int movimento_torre = 5 ;  // define a quantidade de vezes que a peça irá se mover
+        torre (movimento_torre);  // chama o procedimento que define a movimentação da peça
 
-
-    // MOVIMENTAÇÂO DO BISPO - ESTURURA DE REPETIÇÂO "WHILE"
-
-    printf ("\nBISPO cinco casas na diagonal para cima e à direita.\n");
-
-    int bispo = 1; // variavel precisa ser declarada fora da estrutura
-
-    while ( bispo <= 5 ) // condição é testada antes da execução do codigo. caso falso o codigo não é executado.
-    {
-        printf ("Cima, Direita\n");
-        bispo++; // incremento ++ garante que a variante atinja um resultado falso, evitando assim um loop infinito.
-    }
-    
-
-
-    // MOVIMENTAÇÂO DA RAINHA - ESTRUTURA DE REPETIÇÂO "DO-WHILE"
+    printf ("\nBISPO cinco casas na diagonal para cima e a direita.\n");
+        int movimento_bispo = 5;  // define a quantidade de vezes que a peça irá se mover
+        bispo(movimento_bispo);  // chama o procedimento que define a movimentação da peça
 
     printf ("\nRAINHA oito casas para a esquerda.\n");
-    
-    int rainha = 1;  // variavel precisa ser declarada fora da estrutura
+        int movimento_rainha = 8;  // define a quantidade de vezes que a peça irá se mover
+        rainha (movimento_rainha);  // chama o procedimento que define a movimentação da peça
 
-    do{
-        printf ("Esquerda\n");
-        rainha++;  // incremento ++ garante que a variante atinja um resultado falso, evitando assim um loop infinito.
-    } while ( rainha <= 8);  // condição é testada apos a execução do codigo, garantido assim pelo monos 1 execução.
-    
-
-
-     // MOVIMENTAÇÂO DO CAVALO - LOOP ANINHADO ( do-while e for)
-
-     printf ("\nCAVALO duas casas para baixo e uma para esquerda.\n");
-
-     int cavalo = 1 ;  // variavel declarada fora da extrutura para ser utilizada no loop externo ( do-while )
-  
-     do {  // loop externo: responsavel pela segunda parte da movimentação do cavalo "Esquerda", será executado apos a conclusão do loop interno
- 
-         for (int i = 1; i <=2 ; i++)  // loop interno: responsavel pela primeira parte da movimentação do cavalo "baixo". Será executado 2 vezes.
-         {
-             printf("Baixo\n");
-         }    
-          
-         printf("Esquerda\n")  ;
-         cavalo++; // incremento para garantir a finalização do loop.
-         
-     } while ( cavalo <= 1 ); // condição para execução do loop externo
-    
-
-    return 0;
-
+    printf ("\nCAVALO duas casas para cima e uma para direita.\n");
+        int movimento_cavalo = 1;  // define a quantidade de vezes que a peça irá se mover
+        cavalo (movimento_cavalo);  // chama o procedimento que define a movimentação da peça
+return 0;
 }
